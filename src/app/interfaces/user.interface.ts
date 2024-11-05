cc0ca2f73d75e6a453e0decaef791c253115fc79
LucasPagani <100729440+LucasPagani@users.noreply.github.com>
2024-11-05 09:17:35 +0100

    modificacion anonces,forgotPass e interfaces

---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,29 @@
+export interface User {
+  photo: string;
+  isBlocked: any;
+  id_user: number;
+  city: string;
+  email: string;
+  last_name1: string;
+  last_name2?: string | null;
+  last_password_change_at: Date;
+  local_date_time?: Date | null;
+  name: string;
+  password: string;
+  password2?: string | null;
+  phone: string;
+  postal_code: string;
+  profile_photo?: string | null;
+  street: string;
+  username: string;
+}
+
+export interface UserLogin {
+  username: string;
+  password: string;
+}
+
+export interface UserSession {
+  username: string;
+  token: string;
+}
